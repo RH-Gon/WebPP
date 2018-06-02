f7e7a1c9125fad7dd6c2cf0885d899e4cfd3099d
RH-Gon <39667743+RH-Gon@users.noreply.github.com>
2018-06-02 15:52:44 +0900

    Delete routine1.js

--- routine1.js
+++
@@ -1,9 +0,0 @@
-var site = document.getElementsByTagName("html");
-function headerResize() 
-    document.getElementsByTagName("header").style.width = site.style.width;
-    site.style.backgroundcolor = "blue";
-    alert("hi");
-}
-function footerResize() {
-    document.getElementsByTagName("footer").style.width = site.style.width;
-}
